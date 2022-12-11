30ab122bc2ec5b034727e9513904fae27ed4e609
Anonymous9995 <118614958+Anonymous9995@users.noreply.github.com>
2022-12-11 13:31:25 -0500

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A simple program to output a line of text
+ *
+ * Return: 0 on completion
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
